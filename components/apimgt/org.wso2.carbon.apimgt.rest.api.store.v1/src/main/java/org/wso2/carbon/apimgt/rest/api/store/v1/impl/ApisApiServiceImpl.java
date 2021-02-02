9c66ba593b3f728bfe720531ae2c4771926ee3e2
Thisal Tennakoon <thisaltennakoon@gmail.com>
2021-02-02 18:13:55 +0530

    added /apis/{apiId}/comments/{commentId}/replies: resource

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/ApisApiServiceImpl.java
@@ -349,6 +349,10 @@ public class ApisApiServiceImpl implements ApisApiService {
         return null;
     }
 
+    public Response getRepliesOfComment(String commentId, String apiId, String xWSO2Tenant, Integer limit, Integer offset, String ifNoneMatch, Boolean includeCommenterInfo, MessageContext messageContext) throws APIManagementException{
+        return null;
+    }
+
     @Override
     public Response deleteComment(String commentId, String apiId, String ifMatch, MessageContext messageContext) throws APIManagementException {
         String requestedTenantDomain = RestApiCommonUtil.getLoggedInUserTenantDomain();
